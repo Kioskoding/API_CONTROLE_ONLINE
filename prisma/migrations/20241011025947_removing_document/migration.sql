e03e6c5517a469493e34609fd816709736ba3ffb
Patrick Moreira <ptkdev@outlook.com>
2024-11-04 19:26:50 -0300

    adjust prisma

--- prisma/migrations/20241011025947_removing_document/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `documento` on the `Pedido` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Pedido" DROP COLUMN "documento";
